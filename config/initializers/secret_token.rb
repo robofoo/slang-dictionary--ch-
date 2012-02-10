805b702cbae5f81e51bc8d6f78a83fad52ecc65f
Daniel <robotright@gmail.com>
2012-02-10 17:00:54 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrbanChineseDictionary::Application.config.secret_token = 'b881857532e8114bb584a785319fe0e514c9ea8a1b5284060d9e2c9dc417f1e4e5deb173708c6fd354c87cef46906f552d2163348a384c40b66d08cfb059f4fc'
