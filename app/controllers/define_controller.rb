64e83071778b463d4640c858c7b988404f3d53eb
Daniel <robotright@gmail.com>
2012-03-01 21:47:55 +0800

    formatting

--- app/controllers/define_controller.rb
+++ app/controllers/define_controller.rb
@@ -2,6 +2,7 @@ class DefineController < ApplicationController
   def show
     # from letter menu
     @char = params[:char]
+
     if @char
       @definitions = Definition.where(:word => @char..@char.next)
     # from search
