0e5919d1a940b67bc6cafac8f8a9caed76a7355c
Daniel <robotright@gmail.com>
2012-03-03 23:07:07 +0800

    formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
