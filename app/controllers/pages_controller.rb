45321a5f9e2fb538301585103d114c2182833e7a
Daniel <robotright@gmail.com>
2012-03-03 23:33:18 +0800

    show random number of defs on landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @definitions = Definition.offset(rand(Definition.count))
+    random_per_page = 3
+    def_count = Definition.count
+
+    if def_count > random_per_page
+      @definitions = Definition.offset(rand(def_count - (random_per_page - 1)))[0..(random_per_page - 1)]
+    else
+      @definitions = Definition.all
+    end
   end
 end
