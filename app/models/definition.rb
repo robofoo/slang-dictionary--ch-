3a72e5b39d2102994ed2a75a2cfb61a1112925b4
Daniel <robotright@gmail.com>
2012-03-04 21:20:05 +0800

    anon users can submit definitions
    but requires an email and code must be activated in order to make
    definition visible on website

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -2,4 +2,12 @@ class Definition < ActiveRecord::Base
   validates_presence_of :word, :pinyin, :definition, :example, :email
 
   default_scope where(:visible => true)
+
+  after_create :create_code
+
+  private
+
+  def create_code
+    self.code = SecureRandom.hex(6)
+  end
 end
--- spec/models/definition_spec.rb
+++ spec/models/definition_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Definition do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "anon user" do
+    describe "create new definition" do
+      let(:word) { Factory.create(:definition) }
+
+      it 'creates code' do
+        word.code.should_not == nil
+      end
+
+      it 'has an email address' do
+        word.email.should_not == nil
+      end
+    end
+  end
 end
