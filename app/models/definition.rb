1734371e53083777c32b93afae8bfbd87357943b
Daniel <robotright@gmail.com>
2012-03-06 21:56:04 +0800

    for some reason no longer need to manually set visiblity

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -1,15 +1,9 @@
 class Definition < ActiveRecord::Base
   validates_presence_of :word, :pinyin, :definition, :example, :email
 
-  before_create :set_defaults, :create_code
+  before_create :create_code
 
   private
-
-  def set_defaults
-    self.visible = false
-    # returning false would prevent save so return nil
-    nil
-  end
 
   def create_code
     self.code = SecureRandom.hex(6)
