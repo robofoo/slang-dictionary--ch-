5f996a1950e0bbbca3aa1a51246e784a70af3ffa
Daniel <robotright@gmail.com>
2012-03-06 22:07:22 +0800

    restore default_scope and manually set visiblity

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -1,9 +1,16 @@
 class Definition < ActiveRecord::Base
   validates_presence_of :word, :pinyin, :definition, :example, :email
-
-  before_create :create_code
+  default_scope where(:visible => true)
+  before_create :set_defaults, :create_code
 
   private
+
+  def set_defaults
+    # must set manually because of default_scope
+    self.visible = false
+    # returning false would prevent save so return nil
+    nil
+  end
 
   def create_code
     self.code = SecureRandom.hex(6)
