3643d31d6fd0098b2aa9e99e638b721a43ed2ff0
Daniel <robotright@gmail.com>
2012-03-19 20:59:24 +0800

    only show 'reviewed' words

--- app/controllers/define_controller.rb
+++ app/controllers/define_controller.rb
@@ -4,13 +4,13 @@ class DefineController < ApplicationController
     @char = params[:char]
 
     if @char
-      chinese = Definition.where(:word => @char..@char.next)
-      pinyin = Definition.where(:pinyin => @char..@char.next)
+      chinese = Definition.where(:word => @char..@char.next, :status => 'reviewed')
+      pinyin = Definition.where(:pinyin => @char..@char.next, :status => 'reviewed')
       @definitions = chinese + pinyin
     # from search
     else
-      chinese = Definition.where(:word => params[:word])
-      pinyin = Definition.where(:pinyin => params[:word])
+      chinese = Definition.where(:word => params[:word], :status => 'reviewed')
+      pinyin = Definition.where(:pinyin => params[:word], :status => 'reviewed')
       @definitions = chinese + pinyin
     end
   end
