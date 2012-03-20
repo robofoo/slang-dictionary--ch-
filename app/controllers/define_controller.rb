8af3f342b6ae6978f9101103273f3782871e8fc9
Daniel <robotright@gmail.com>
2012-03-20 15:14:08 +0800

    search correct field with correct format

--- app/controllers/define_controller.rb
+++ app/controllers/define_controller.rb
@@ -5,12 +5,13 @@ class DefineController < ApplicationController
 
     if @char
       chinese = Definition.where(:word => @char..@char.next, :status => 'reviewed')
-      pinyin = Definition.where(:pinyin => @char..@char.next, :status => 'reviewed')
+      pinyin = Definition.where(:pinyin_for_search => @char..@char.next, :status => 'reviewed')
       @definitions = chinese + pinyin
     # from search
     else
       chinese = Definition.where(:word => params[:word], :status => 'reviewed')
-      pinyin = Definition.where(:pinyin => params[:word], :status => 'reviewed')
+      # pinyin_for_search has spaces stripped
+      pinyin = Definition.where(:pinyin_for_search => params[:word].gsub(' ',''), :status => 'reviewed')
       @definitions = chinese + pinyin
     end
   end
