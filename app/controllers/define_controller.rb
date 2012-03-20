ff87ee5c00c12dad7e23eddff9ee67378fee45cb
Daniel <robotright@gmail.com>
2012-03-20 18:24:08 +0800

    use wildcard for searches

--- app/controllers/define_controller.rb
+++ app/controllers/define_controller.rb
@@ -8,9 +8,9 @@ class DefineController < ApplicationController
       pinyin = Definition.where(:pinyin_for_search => @char..@char.next, :status => 'reviewed')
     # from search
     else
-      chinese = Definition.where(:word => params[:word], :status => 'reviewed')
+      chinese = Definition.where('word like ?', params[:word] + "%").where(:status => 'reviewed')
       # pinyin_for_search has spaces stripped
-      pinyin = Definition.where(:pinyin_for_search => params[:word].gsub(' ',''), :status => 'reviewed')
+      pinyin = Definition.where('pinyin_for_search like ?', params[:word].gsub(' ','') + "%").where(:status => 'reviewed')
     end
 
     @definitions = chinese + pinyin
