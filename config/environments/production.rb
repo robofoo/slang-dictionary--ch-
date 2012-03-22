ad96cf947f9999386f07df671111c045e9e19d16
Daniel <robotright@gmail.com>
2012-03-22 21:39:29 +0800

    correct domain for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ UrbanChineseDictionary::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # for devise
-  config.action_mailer.default_url_options = { :host => 'cudslang.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'chengshicidian.com' }
 end
